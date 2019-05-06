f1ee30a3cfea37861dbf2fc8a2dddd9e525b109d
Markus Sonderegger <marraison@gmail.com>
2019-05-06 18:17:10 +0200

    make qid types public

--- proto/unix.go
+++ proto/unix.go
@@ -18,28 +18,30 @@ func decodeTimespec(buf *binary.Buffer) unix.Timespec {
 	}
 }
 
+// UnixDirTypeToQidType converts an unix directory type.
 func UnixDirTypeToQidType(typ uint8) uint8 {
 	switch typ {
 	//case unix.DT_BLK: ???
 	case unix.DT_CHR, unix.DT_FIFO, unix.DT_SOCK:
-		return typeAppendOnly
+		return TypeAppendOnly
 	case unix.DT_DIR:
-		return typeDirectory
+		return TypeDirectory
 	case unix.DT_LNK:
-		return typeSymlink
+		return TypeSymlink
 	}
-	return typeRegular
+	return TypeRegular
 }
 
+// UnixFileTypeToQidType converts an unix file type.
 func UnixFileTypeToQidType(mode uint32) uint8 {
 	switch mode & unix.S_IFMT {
 	//case unix.S_IFBLK: ???
 	case unix.S_IFCHR, unix.S_IFIFO, unix.S_IFSOCK:
-		return typeAppendOnly
+		return TypeAppendOnly
 	case unix.S_IFDIR:
-		return typeDirectory
+		return TypeDirectory
 	case unix.S_IFLNK:
-		return typeSymlink
+		return TypeSymlink
 	}
-	return typeRegular
+	return TypeRegular
 }
