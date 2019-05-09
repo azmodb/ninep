24740c51e723283bb0d35ee47a8e36e4925992a0
Markus Sonderegger <marraison@gmail.com>
2019-05-09 20:35:03 +0200

    implement dirent type

--- proto/unix.go
+++ proto/unix.go
@@ -1,9 +1,81 @@
 package proto
 
 import (
+	"fmt"
+	"io"
+
 	"github.com/azmodb/ninep/binary"
 	"golang.org/x/sys/unix"
 )
+
+// Dirent structure defines the format of 9P2000.L directory entries.
+type Dirent struct {
+	Qid
+	Offset uint64
+	Type   uint8
+	Name   string
+}
+
+// String implements fmt.Stringer.
+func (m Dirent) String() string {
+	return fmt.Sprintf("%s offset:%d type:%d name:%q",
+		m.Qid, m.Offset, m.Type, m.Name)
+}
+
+// Len returns the length of the message in bytes.
+func (m Dirent) Len() int { return 13 + 8 + 1 + 2 + len(m.Name) }
+
+// Reset resets all state.
+func (m *Dirent) Reset() { *m = Dirent{} }
+
+// Encode encodes to the given binary.Buffer.
+func (m Dirent) Encode(buf *binary.Buffer) {
+	m.Qid.Encode(buf)
+	buf.PutUint64(m.Offset)
+	buf.PutUint8(m.Type)
+	buf.PutString(m.Name)
+}
+
+// Decode decodes from the given binary.Buffer.
+func (m *Dirent) Decode(buf *binary.Buffer) {
+	m.Qid.Decode(buf)
+	m.Offset = buf.Uint64()
+	m.Type = buf.Uint8()
+	m.Name = buf.String()
+}
+
+// Marshal implements binary.Marshaler.
+func (m Dirent) Marshal(data []byte) ([]byte, int, error) {
+	data = binary.PutUint8(data, m.Qid.Type)
+	data = binary.PutUint32(data, m.Qid.Version)
+	data = binary.PutUint64(data, m.Qid.Path)
+	data = binary.PutUint64(data, m.Offset)
+	data = binary.PutUint8(data, m.Type)
+	data = binary.PutString(data, m.Name)
+	return data, m.Len(), nil
+}
+
+// Unmarshal implements binary.Unmarshaler.
+func (m *Dirent) Unmarshal(data []byte) ([]byte, error) {
+	if len(data) < 24 {
+		return data, io.ErrUnexpectedEOF
+	}
+	m.Qid = Qid{
+		Type:    binary.Uint8(data[:1]),
+		Version: binary.Uint32(data[1:5]),
+		Path:    binary.Uint64(data[5:13]),
+	}
+	m.Offset = binary.Uint64(data[13:21])
+	m.Type = binary.Uint8(data[21:22])
+	m.Name = binary.String(data[22:])
+	return data, nil
+}
+
+// Stat describes a file system object.
+type Stat = unix.Stat_t
+
+// StatFS describes a file system.
+type StatFS = unix.Statfs_t
 
 func encodeTimespec(buf *binary.Buffer, t unix.Timespec) {
 	sec, nsec := t.Unix()
