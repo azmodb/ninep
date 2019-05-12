5ae64808caf8e56fbbbe90a714504ff3cdfdab93
Markus Sonderegger <marraison@gmail.com>
2019-05-12 11:50:20 +0200

    add 9P2000.L FileMode and Flags type

--- proto/unix.go
+++ proto/unix.go
@@ -3,6 +3,7 @@ package proto
 import (
 	"fmt"
 	"io"
+	"os"
 
 	"github.com/azmodb/ninep/binary"
 	"golang.org/x/sys/unix"
@@ -115,4 +116,298 @@ func UnixFileTypeToQidType(mode uint32) uint8 {
 		return TypeSymlink
 	}
 	return TypeRegular
+}
+
+// FileMode are flags corresponding to file modes. These correspond to
+// bits sent over the wire and also to mode_t bits.
+type FileMode uint32
+
+// Flags corresponding to file modes. These correspond to bits sent over
+// the wire and also correspond to mode_t bits.
+const (
+	// FileModeMask is a mask of all the file mode bits of FileMode.
+	FileModeMask FileMode = 0170000
+
+	// ModeRegular is a mode bit for regular files.
+	ModeRegular FileMode = 0100000
+
+	// ModeDirectory is a mode bit for directories.
+	ModeDirectory FileMode = 040000
+
+	// ModeSymlink is a mode bit for a symlink.
+	ModeSymlink FileMode = 0120000
+
+	// ModeBlockDevice is a mode bit for block devices.
+	ModeBlockDevice FileMode = 060000
+
+	// ModeCharacterDevice is a mode bit for a character device.
+	ModeCharacterDevice FileMode = 020000
+
+	// ModeSocket is an mode bit for a socket.
+	ModeSocket FileMode = 0140000
+
+	// ModeNamedPipe is a mode bit for a named pipe.
+	ModeNamedPipe FileMode = 010000
+
+	// ModeGroupID has several special uses. For a directory, it
+	// indicates that BSD semantics is to be used for that directory:
+	// files created there inherit their group ID from the directory,
+	// not from the effective group ID of the creating process, and
+	// directories created there will also get the ModeGroupID bit
+	// set.
+	ModeGroupID FileMode = 0x400
+	ModeUserID  FileMode = 0x800
+	ModeSticky  FileMode = 0x200
+
+	ModePerm = 0777 // Unix permission bits
+)
+
+// NewFileMode returns a FileMode from an os.FileMode.
+func NewFileMode(mode os.FileMode) FileMode {
+	m := FileMode(mode & os.ModePerm)
+
+	switch {
+	case mode&os.ModeCharDevice != 0:
+		m |= ModeCharacterDevice
+	case mode&os.ModeDevice != 0:
+		m |= ModeBlockDevice
+	case mode&os.ModeNamedPipe != 0:
+		m |= ModeNamedPipe
+	case mode&os.ModeSymlink != 0:
+		m |= ModeSymlink
+	case mode&os.ModeSocket != 0:
+		m |= ModeSocket
+	case mode&os.ModeDir != 0:
+		m |= ModeDirectory
+	default:
+		m |= ModeRegular
+	}
+
+	if mode&os.ModeSetgid != 0 {
+		m |= ModeGroupID
+	}
+	if mode&os.ModeSetuid != 0 {
+		m |= ModeUserID
+	}
+	if mode&os.ModeSticky != 0 {
+		m |= ModeSticky
+	}
+	return m
+}
+
+// NewFileModeUnix returns a FileMode from a mode_t.
+func NewFileModeUnix(mode uint32) FileMode {
+	m := FileMode(mode & ModePerm)
+
+	switch mode & unix.S_IFMT {
+	case unix.S_IFCHR:
+		m |= ModeCharacterDevice
+	case unix.S_IFBLK:
+		m |= ModeBlockDevice
+	case unix.S_IFIFO:
+		m |= ModeNamedPipe
+	case unix.S_IFLNK:
+		m |= ModeSymlink
+	case unix.S_IFSOCK:
+		m |= ModeSocket
+	case unix.S_IFDIR:
+		m |= ModeDirectory
+	default:
+		m |= ModeRegular
+	}
+
+	if mode&unix.S_ISGID != 0 {
+		m |= ModeGroupID
+	}
+	if mode&unix.S_ISUID != 0 {
+		m |= ModeUserID
+	}
+	if mode&unix.S_ISVTX != 0 {
+		m |= ModeSticky
+	}
+	return m
+}
+
+// FileMode converts a 9P2000.L file mode to an os.FileMode.
+func (m FileMode) FileMode() os.FileMode {
+	mode := os.FileMode(m & 0777)
+
+	switch m & FileModeMask {
+	case ModeCharacterDevice:
+		mode |= os.ModeDevice | os.ModeCharDevice
+	case ModeBlockDevice:
+		mode |= os.ModeDevice
+	case ModeNamedPipe:
+		mode |= os.ModeNamedPipe
+	case ModeSymlink:
+		mode |= os.ModeSymlink
+	case ModeSocket:
+		mode |= os.ModeSocket
+	case ModeDirectory:
+		mode |= os.ModeDir
+	case ModeRegular:
+		// nothing to do
+	}
+
+	if m&ModeGroupID != 0 {
+		mode |= os.ModeSetgid
+	}
+	if m&ModeUserID != 0 {
+		mode |= os.ModeSetuid
+	}
+	if m&ModeSticky != 0 {
+		mode |= os.ModeSticky
+	}
+	return mode
+}
+
+const rwx = "rwxrwxrwx"
+
+func (m FileMode) String() string {
+	mask := m & FileModeMask
+	buf := [32]byte{}
+	w := 0
+
+	if mask == ModeCharacterDevice {
+		buf[w], w = 'D', w+1
+		buf[w], w = 'c', w+1
+	}
+	if mask == ModeBlockDevice {
+		buf[w], w = 'D', w+1
+	}
+	if mask == ModeDirectory {
+		buf[w], w = 'd', w+1
+	}
+	if mask == ModeSymlink {
+		buf[w], w = 'L', w+1
+	}
+	if mask == ModeSocket {
+		buf[w], w = 'S', w+1
+	}
+	if mask == ModeNamedPipe {
+		buf[w], w = 'p', w+1
+	}
+
+	if m&ModeGroupID != 0 {
+		buf[w], w = 'g', w+1
+	}
+	if m&ModeUserID != 0 {
+		buf[w], w = 'u', w+1
+	}
+	if m&ModeSticky != 0 {
+		buf[w], w = 't', w+1
+	}
+	if w == 0 {
+		buf[w] = '-'
+		w++
+	}
+
+	for i, c := range rwx {
+		if m&(1<<uint(9-1-i)) != 0 {
+			buf[w] = byte(c)
+		} else {
+			buf[w] = '-'
+		}
+		w++
+	}
+	return string(buf[:w])
+}
+
+// Flag is the mode passed to Open and Create operations. These
+// correspond to bits sent over the wire.
+type Flag uint32
+
+const (
+	// FlagModeMask is a mask of valid Flag mode bits.
+	FlagModeMask Flag = 0x3
+
+	// FlagReadOnly flag is indicating read-only mode.
+	FlagReadOnly Flag = 0x0
+
+	// FlagWriteOnly flag is indicating write-only mode.
+	FlagWriteOnly Flag = 0x1
+
+	// FlagReadWrite flag is indicating read-write mode.
+	FlagReadWrite Flag = 0x2
+
+	// FlagAppend opens the file in append mode. Before each write, the
+	// file offset is positioned at the end of the file. The
+	// modification of the file offset and the write operation are
+	// performed as a single atomic step.
+	FlagAppend Flag = 0x400
+
+	// FlagTruncate is indicating to truncate file when opened, if
+	// possible.
+	FlagTruncate Flag = 0x200
+
+	// FlagSync is indicating to open for synchronous I/O.
+	FlagSync Flag = 0x101000
+
+	// internal used flags
+	flagCreate    Flag = 0x40
+	flagExclusive Flag = 0x80
+)
+
+// NewFlag returns a Flag from an int compatible with open(2).
+func NewFlag(flags int) Flag {
+	f := Flag(flags & unix.O_ACCMODE)
+
+	switch flags & unix.O_ACCMODE {
+	case unix.O_RDONLY:
+		f |= FlagReadOnly
+	case unix.O_RDWR:
+		f |= FlagReadWrite
+	case unix.O_WRONLY:
+		f |= FlagWriteOnly
+	}
+
+	if flags&unix.O_CREAT != 0 {
+		f |= flagCreate
+	}
+	if flags&unix.O_EXCL != 0 {
+		f |= flagExclusive
+	}
+
+	if flags&unix.O_APPEND != 0 {
+		f |= FlagAppend
+	}
+	if flags&unix.O_TRUNC != 0 {
+		f |= FlagTruncate
+	}
+	if flags&unix.O_SYNC != 0 {
+		f |= FlagSync
+	}
+	return f
+}
+
+// Flags converts a Flag to an int compatible with open(2).
+func (f Flag) Flags() int {
+	flags := int(f & FlagModeMask)
+
+	switch f & FlagModeMask {
+	case FlagReadOnly:
+		flags |= unix.O_RDONLY
+	case FlagReadWrite:
+		flags |= unix.O_RDWR
+	case FlagWriteOnly:
+		flags |= unix.O_WRONLY
+	}
+
+	if f&flagCreate != 0 {
+		flags |= unix.O_CREAT
+	}
+	if f&flagExclusive != 0 {
+		flags |= unix.O_EXCL
+	}
+
+	if f&FlagAppend != 0 {
+		flags |= unix.O_APPEND
+	}
+	if f&FlagTruncate != 0 {
+		flags |= unix.O_TRUNC
+	}
+	if f&FlagSync != 0 {
+		flags |= unix.O_SYNC
+	}
+	return flags
 }
---
+++ proto/unix_test.go
@@ -0,0 +1,103 @@
+package proto
+
+import (
+	"os"
+	"testing"
+
+	"golang.org/x/sys/unix"
+)
+
+func TestConvertFileMode(t *testing.T) {
+	for i, mode := range []os.FileMode{
+		os.ModeDevice | os.ModeCharDevice | 0644,
+		os.ModeDir | 0755,
+		os.ModeSymlink | 0644,
+		os.ModeSocket | 0644,
+		os.ModeNamedPipe | 0644,
+		os.ModeDevice | 0644,
+		0664, 0775, 0222,
+
+		os.ModeDir | os.ModeSticky | 0755,
+		os.ModeDir | os.ModeSetgid | 0755,
+		os.ModeDir | os.ModeSetuid | 0755,
+
+		os.ModeDevice | os.ModeCharDevice,
+		os.ModeNamedPipe,
+		os.ModeSocket,
+		os.ModeDevice,
+		os.ModeDir,
+		os.ModeSymlink,
+		0, /* os.ModeRegular */
+	} {
+		m := NewFileMode(mode)
+
+		if mode != m.FileMode() {
+			t.Fatalf("mode converter (#%.4d): expected file mode %d, got %d", i, mode, m.FileMode())
+		}
+
+		v := m.FileMode()
+		if mode.String() != v.String() {
+			t.Fatalf("mode converter (#%.4d): expected file mode %q, got %q", i, mode, v)
+		}
+
+		if mode.String() != m.String() {
+			t.Fatalf("mode converter (#%.4d): expected file mode %q, got %q", i, mode, m)
+		}
+	}
+}
+
+func TestConvertFileModePosix(t *testing.T) {
+	for i, test := range []struct {
+		mode uint32
+		want FileMode
+	}{
+		{unix.S_IFDIR | unix.S_ISGID | 0755, ModeDirectory | ModeGroupID | 0755},
+		{unix.S_IFDIR | unix.S_ISUID | 0755, ModeDirectory | ModeUserID | 0755},
+		{unix.S_IFDIR | unix.S_ISVTX | 0755, ModeDirectory | ModeSticky | 0755},
+
+		{unix.S_IFCHR | 0644, ModeCharacterDevice | 0644},
+		{unix.S_IFIFO | 0644, ModeNamedPipe | 0644},
+		{unix.S_IFDIR | 0644, ModeDirectory | 0644},
+		{unix.S_IFBLK | 0644, ModeBlockDevice | 0644},
+		{unix.S_IFSOCK | 0644, ModeSocket | 0644},
+		{unix.S_IFLNK | 0644, ModeSymlink | 0644},
+		{unix.S_IFREG | 0644, ModeRegular | 0644},
+
+		{unix.S_IFCHR, ModeCharacterDevice},
+		{unix.S_IFIFO, ModeNamedPipe},
+		{unix.S_IFDIR, ModeDirectory},
+		{unix.S_IFBLK, ModeBlockDevice},
+		{unix.S_IFSOCK, ModeSocket},
+		{unix.S_IFLNK, ModeSymlink},
+		{unix.S_IFREG, ModeRegular},
+	} {
+		m := NewFileModeUnix(test.mode)
+		if m != test.want {
+			t.Fatalf("mode converter (#%.4d): expected file mode %d, got %d", i, test.want, m)
+		}
+	}
+}
+
+func TestConvertFlag(t *testing.T) {
+	for i, flags := range []int{
+		os.O_APPEND | os.O_WRONLY,
+		os.O_TRUNC | os.O_WRONLY,
+		os.O_SYNC | os.O_WRONLY,
+
+		os.O_RDONLY,
+		os.O_RDWR,
+		os.O_WRONLY,
+
+		os.O_APPEND,
+		os.O_APPEND | os.O_RDWR,
+
+		os.O_CREATE,
+		os.O_EXCL,
+	} {
+		f := NewFlag(flags)
+
+		if flags != f.Flags() {
+			t.Fatalf("flag converter (#%.4d): expected flag %d, got %d", i, flags, f.Flags())
+		}
+	}
+}
