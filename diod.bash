c6b928d21662c60de1e298053b71e0a165ad5be8
Markus Sonderegger <marraison@gmail.com>
2019-05-12 11:51:08 +0200

    add configuration variables

--- diod.bash
+++ diod.bash
@@ -1,10 +1,14 @@
 #!/bin/bash
+
 CONTAINER=ninep-client-test
 IMAGE=marraison/diod:latest
+ADDR=127.0.0.1:5640
+EXPORTDIR=/tmp
 
 if [ ! $(docker ps --quiet --filter name=${CONTAINER}) ]; then
-	docker run --detach --rm --publish 127.0.0.1:5640:5640 \
+	docker run --detach --rm --publish ${ADDR}:5640 \
 		--name ${CONTAINER} ${IMAGE} \
-			--export /tmp --no-auth \
+			--export ${EXPORTDIR} \
+			--no-auth \
 			--debug 3 --logdest stderr
 fi
