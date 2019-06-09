7d08912d0815a3f1254a54942de83072951451ac
Markus Sonderegger <marraison@gmail.com>
2019-06-09 12:29:10 +0200

    implement basic readdir tests

--- posix/record_test.go
+++ posix/record_test.go
@@ -1,12 +1,33 @@
 package posix
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"golang.org/x/sys/unix"
 )
+
+func checkRecords(t *testing.T, records []Record, num int, want map[string]uint8) {
+	if len(records) != num {
+		t.Fatalf("ReadDir: expected %d records, got %d", num, len(records))
+	}
+
+	for _, rec := range records {
+		typ, found := want[rec.Name]
+		if !found {
+			t.Fatalf("ReadDir: found unexpected name %q", rec.Name)
+		}
+		if typ != rec.Type {
+			t.Fatalf("ReadDir: expected dirent type %d, got %d", rec.Type, typ)
+		}
+
+		//if rec.Offset != uint64(i+1) {
+		//	t.Fatalf("ReadDir: expected offset %d, got %d", i+1, rec.Offset)
+		//}
+	}
+}
 
 var expectedRecords = map[string]uint8{
 	".":     unix.DT_DIR,
@@ -21,30 +42,62 @@ var expectedRecords = map[string]uint8{
 func TestReadDir(t *testing.T) {
 	dir, err := os.Open(filepath.Join("testdata", "dirent"))
 	if err != nil {
-		t.Fatalf("redDir: %v", err)
+		t.Fatalf("readDir: %v", err)
 	}
 	defer dir.Close()
 
 	records, err := readDir(int(dir.Fd()))
 	if err != nil {
-		t.Fatalf("redDir: %v", err)
+		t.Fatalf("readDir: %v", err)
 	}
 
-	if len(records) != len(expectedRecords) {
-		t.Fatalf("redDir: expected %d records, got %d", len(expectedRecords), len(records))
+	checkRecords(t, records, len(expectedRecords), expectedRecords)
+}
+
+func TestReadDirFull(t *testing.T) {
+	f, err := os.Open(filepath.Join("testdata", "dirent"))
+	if err != nil {
+		t.Fatalf("ReadDir: %v", err)
+	}
+	dir := &pfile{f: f}
+	defer dir.Close()
+
+	records, err := dir.ReadDir(0)
+	if err != nil {
+		t.Errorf("ReadDir: unexpected error: %v", err)
 	}
 
-	for i, entry := range records {
-		typ, found := expectedRecords[entry.Name]
-		if !found {
-			t.Fatalf("redDir: found unexpected name %q", entry.Name)
-		}
-		if typ != entry.Type {
-			t.Fatalf("redDir: expected dirent type %d, got %d", entry.Type, typ)
+	checkRecords(t, records, len(expectedRecords), expectedRecords)
+}
+
+func TestReadDirOffset(t *testing.T) {
+	f, err := os.Open(filepath.Join("testdata", "dirent"))
+	if err != nil {
+		t.Fatalf("ReadDir: %v", err)
+	}
+	dir := &pfile{f: f}
+	defer dir.Close()
+
+	for _, test := range []struct {
+		offset int64
+
+		err error
+	}{
+		{0, nil},
+		{1, nil},
+		{2, nil},
+		{3, nil},
+		{4, nil},
+		{5, nil},
+		{6, nil},
+		{7, io.EOF},
+	} {
+		records, err := dir.ReadDir(test.offset)
+		if err != test.err {
+			t.Errorf("ReadDir: unexpected error: %v", err)
 		}
 
-		if entry.Offset != uint64(i+1) {
-			t.Fatalf("redDir: expected offset %d, got %d", i+1, entry.Offset)
-		}
+		want := len(expectedRecords) - int(test.offset)
+		checkRecords(t, records, want, expectedRecords)
 	}
 }
